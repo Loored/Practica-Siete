ab836b4c169fec1c8dba94b29de6f278df6de9f1
Eduardo Juárez Sánchez <37821778+Loored@users.noreply.github.com>
2018-05-30 21:22:23 -0500

    Afinamiento codigo
    Cambio de nombre de variables. Agrego funciones y agrego el array en donde se almacenan los simbolos que seran desplegados en el LCD.

--- agrego_funciones.c
+++ agrego_funciones.c
@@ -5,26 +5,25 @@
 #include <lcd420.c>
 #define DHT11_pin PIN_A0
 
-unsigned char values[5];
+unsigned char valores[5];
 void DHT11_init();
-unsigned char get_byte();
-unsigned char get_data();
-void setup();
-void lcd_symbols();
+unsigned char obtenerByte();
+unsigned char obtenerDatos();
+void configuracionLCD();
+void simbolosLCD();
 
-void DHT11_init()
+const unsigned char simbolos[8]=
 {
-    output_float(DHT11_pin);
-    delay_ms(1000);
-}
+    0x07, 0x05, 0x05, 0x07, 0x00, 0x00, 0x00, 0x00
+};
 
 void main()
 {
     unsigned char estado = 0;
-    setup();
+    configuracionLCD();
     while(TRUE)
     {
-        estado = get_data();
+        estado = obtenerDatos();
         switch(estado)
         {
             case 1:
@@ -47,4 +46,97 @@ void main()
         }
         delay_ms(1000);
     };
+}
+
+void DHT11_init()
+{
+    output_float(DHT11_pin);
+    delay_ms(1000);
+}
+
+unsigned char obtenerByte()
+{
+    unsigned char lectura = 0;
+    unsigned char valor = 0;
+    
+    for(lectura = 0; lectura < 8; lectura+= 1)
+    {
+        valor <<= 1;
+        while(!input(DHT11_pin));
+        delay_us(30);
+        
+        if(input(DHT11_pin))
+        {
+            valor |= 1;
+        }
+        while(input(DHT11_pin));
+    }
+    return valor;
+}
+
+unsigned char obtenerDatos()
+{
+    short entrada = 0;
+    unsigned char lectura = 0;
+    unsigned char checksum = 0;
+    
+    output_high(DHT11_pin);
+    output_low(DHT11_pin);
+    delay_ms(18);
+    output_high(DHT11_pin);
+    delay_us(26);
+    
+    entrada = input(DHT11_pin);
+    if(!entrada)
+    {
+        return 1;
+    }
+    delay_us(80);
+    entrada = input(DHT11_pin);
+    if(!entrada)
+    {
+        return 2;
+    }
+    delay_us(80);
+    
+    for(lectura = 0; lectura < 4; lectura+= 1)
+    {
+        valores[lectura] = obtenerByte();
+    }
+    output_high(DHT11_pin);
+    for(lectura = 0; lectura < 4; lectura+= 1)
+    {
+        checksum += valores[lectura];
+    }
+    if(checksum != valores[4])
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void configuracionLCD()
+{
+    disable_interrupts(GLOBAL);
+    //port_B_pullups(FALSE);
+    setup_timer_0(T0_INTERNAL); //| T0_8_bit);
+    set_timer0(0);
+    DHT11_init();
+    lcd_init();
+    lcd_putc("\f");
+    simbolosLCD();
+}
+
+void simbolosLCD()
+{
+    unsigned char envio = 0;
+    lcd_send_byte(0, 0x40);
+    for(envio = 0; envio < 8; envio++)
+    {
+        lcd_send_byte(1, simbolos[envio]);
+    }
+    lcd_send_byte(0, 0x80);
 }
