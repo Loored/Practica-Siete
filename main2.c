9d9789175c1077a42da18a6e7c83dbbe3d44f685
Eduardo Juárez Sánchez <37821778+Loored@users.noreply.github.com>
2018-05-30 19:57:10 -0500

    Template inicial
    Librerias necesarias asi como fusibles, se crea el vector en el que se guardaran los valores y se aniaden prototipos de funciones necesarias. Se creo el bloque de codigo principal en donde se ejecuta el ciclo infinito. Se lee a traves de la variable estado y a partir del dato obtenido se crea un switch.

---
+++ main2.c
@@ -0,0 +1,43 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, STVREN, NOLVP, NODEBUG,
+#use delay (clock = 16000000)
+#include <lcd420.c>
+#define DHT11_pin PIN_A0
+
+unsigned char values[5];
+void DHT11_init();
+unsigned char get_byte();
+unsigned char get_data();
+
+
+void main()
+{
+    unsigned char estado = 0;
+    setup();
+    while(TRUE)
+    {
+        estado = get_data();
+        switch(estado)
+        {
+            case 1:
+            {
+                
+            }
+            case 2:
+            {
+                break;
+            }
+            case 3:
+            {
+                break;
+            }
+            default:
+            {
+                
+                break;
+            }
+        }
+        delay_ms(1000);
+    };
+}
